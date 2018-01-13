375fe1fb8b1978141fb89104ae54ec99aa4b283c
Parker Catalano <catalano.work@gmail.com>
2018-01-13 23:02:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,9 +63,7 @@ class Dog
     SQL
 binding.pry
     dog = DB[:conn].execute(sql, id)[0]
-    new_dog = self.new(name:, breed:)
-    self.id = dog[0]
-    self.name = dog
+    new_dog = self.create
   end
 
   def self.find_or_create_by(name:, breed:)
