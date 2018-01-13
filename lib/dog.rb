fd9f93102e44b05c2b0d7659b1067e3bd6f12969
Parker Catalano <catalano.work@gmail.com>
2018-01-13 23:05:44 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,6 +64,7 @@ class Dog
 
     dog = DB[:conn].execute(sql, id)[0]
     new_dog = self.new(id: dog[0], name: dog[1], breed: dog[2])
+    new_dog.save
   end
 
   def self.find_or_create_by(name:, breed:)
