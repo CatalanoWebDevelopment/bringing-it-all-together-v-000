6c76f4a3f7711db7abdeb16cfac73269d31cf709
Parker Catalano <catalano.work@gmail.com>
2018-01-13 22:47:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,6 +68,10 @@ class Dog
   def self.find_or_create_by(name:, breed:)
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
 
+    if !dog.empty?
+      dog_data = [0]
+    end
+  end
 
 
 end
