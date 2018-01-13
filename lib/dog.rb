fc5b44850be35b0f05ff1cd3a6bbf9646019f7fe
Parker Catalano <catalano.work@gmail.com>
2018-01-13 22:54:15 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,8 +51,8 @@ class Dog
     end
   end
 
-  def self.create(name: name, breed:)
-    new_dog = Dog.new(name, breed)
+  def self.create(name:, breed:)
+    new_dog = Dog.new(name: name, breed)
     new_dog.save
     new_dog
   end
