ec0293c0da1d4568b06425659c5af8e32e94cc64
Parker Catalano <catalano.work@gmail.com>
2018-01-13 23:15:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -71,7 +71,12 @@ class Dog
 
     if !dog.empty?
       dog_data = [0]
-      dog = self.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2]) 
+      dog = self.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
+    else
+      dog = self.create(name: name, breed: breed)
+    end
+    dog
+  end 
 
 
 
